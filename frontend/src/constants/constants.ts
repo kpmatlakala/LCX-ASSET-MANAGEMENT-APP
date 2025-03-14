75e99e8d0228a42d560476532bfa83916052618a
frankiescot788 <scottfrankie864@gmail.com>
2025-03-14 14:30:00 +0200

    an initial commit that includes most of the core features

---
+++ frontend/src/constants/constants.ts
@@ -0,0 +1,26 @@
+export const assetTypes: { type: string; value: string }[] = [
+  { type: "Laptop", value: "Laptop" },
+  { type: "Computer Case", value: "Desktop" },
+  { type: "Monitor", value: "Monitor" },
+  { type: "Printer", value: "Printer" },
+  { type: "Router", value: "Router" },
+  { type: "Switch", value: "Switch" },
+  { type: "Server", value: "Server" },
+  { type: "Tablet", value: "Tablet" },
+  { type: "Smartphone", value: "Smartphone" },
+  { type: "Projector", value: "Projector" },
+  { type: "Scanner", value: "Scanner" },
+  { type: "Keyboard", value: "Keyboard" },
+  { type: "Mouse", value: "Mouse" },
+];
+
+export const categories = [
+  "Electronics",
+  "Furniture",
+  "Vehicle",
+  "Software",
+  "Equipment",
+];
+export const conditions = ["New", "Good", "Fair", "Poor", "Damaged"];
+
+export const fileTypes = ["JPG", "PNG", "GIF"];
