6af17a39740ee002f614b0d627c097280b324e34
DeLightPlus <matlakalakabelo1@gmail.com>
2025-03-20 11:51:20 +0200

    prepare for merge

--- _/backend/src/utils/scheduler.ts
+++
@@ -1,8 +0,0 @@
-import cron from "node-cron";
-import { supabase } from "../lib/supabase";
-
-export default function StartCronJobs(): void {
-    cron.schedule("*/30 * * * *", async () => {
-        await supabase.from("email_otps").delete().lt("expires_at", new Date().toISOString());
-    })
-}
